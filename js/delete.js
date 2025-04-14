26381e1cde32cc393cfb241ac6e4a3d9e53f9767
rmfTp <kyj2001kyj@naver.com>
2025-04-14 15:52:42 +0900

    '5'

---
+++ js/delete.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded',function(){
+    removeEl(seq){
+        const index = this.items.findIndex((item) => item.seq = seq);
+        if (index !== -1) {
+            this.items.splice(index, 1);
+            this.save();
+        }
+    }
+    const removeEl = el.querySelector('.remove');
+    removeEl.addEventListener('click', () => {
+        if (confirm('정말 삭제하겠습니까?')) {
+            this.remove(seq)
+        }
+    });
+})
